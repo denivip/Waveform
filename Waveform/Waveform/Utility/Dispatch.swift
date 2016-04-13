9be530420eebf88d35cfedf41ec98ffdee2f6bcc
AntonBelousov <antbelousov@gmail.com>
2016-04-13 16:28:46 +0300

    Refactoring. Changed default dispatch label

--- Waveform/Waveform/Utility/Dispatch.swift
+++ Waveform/Waveform/Utility/Dispatch.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let processingQueue = dispatch_queue_create("ru.denivip.denoise.processing", DISPATCH_QUEUE_SERIAL)
+let processingQueue = dispatch_queue_create("ru.denivip.waveform.processing", DISPATCH_QUEUE_SERIAL)
 
 public func runAsynchronouslyOnProcessingQueue(block: dispatch_block_t) {
     if (dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL) == dispatch_queue_get_label(self.processingQueue)) {
